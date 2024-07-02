1ef7abcde3062f0915acd67e2f81143ef5ed53c9
Ethan Walker <ethan.walker@techcyte.com>
2024-07-02 16:11:02 -0700

    add if err != nil keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -205,6 +205,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- [[ golang specific keymap ]]
+vim.keymap.set('n', '<leader>en', 'iif err != nil {}<Esc>f{', { desc = 'Insert if err != nil {}' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
