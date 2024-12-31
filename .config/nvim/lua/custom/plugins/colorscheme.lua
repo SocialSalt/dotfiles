907edde934a68e09d88baf18dc854b64caab5900
Ethan Walker <ethan.walker830@gmail.com>
2024-12-31 11:00:02 -0800

    finalize ghostty config

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
         compile_file_suffix = "_compiled", -- Compiled file suffix
         transparent = false, -- Disable setting background
         terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-        dim_inactive = true, -- Non focused panes set to alternative background
+        dim_inactive = false, -- Non focused panes set to alternative background
       },
       palettes = {
         nightfox = {
