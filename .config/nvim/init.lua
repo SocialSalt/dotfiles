78e6eec585b858bd8ddd49ec51058f0b3b4a3860
Ethan Walker <ethan.walker@techcyte.com>
2025-09-16 15:42:47 -0700

    add codecompanion setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1142,6 +1142,43 @@ require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
+  -- AI tools
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("codecompanion").setup({
+        log_level = "DEBUG",
+        strategies = {
+          chat = {
+            adapter = "gemini_cli",
+          },
+          inline = {
+            adapter = "gemini_cli",
+          },
+          cmd = {
+            adapter = "gemini_cli",
+          },
+        },
+        adapters = {
+          acp = {
+            gemini_cli = function()
+              return require("codecompanion.adapters").extend("gemini_cli", {
+                env = {
+                  api_key = "GEMINI_API_KEY",
+                },
+              })
+            end,
+          },
+        },
+      })
+      vim.keymap.set({ "n" }, "<leader>ch", "<Cmd>CodeCompanionChat<CR>", { desc = "[C]ompanion C[h]at" })
+      vim.keymap.set({ "n" }, "<leader>cc", "<Cmd>CodeCompanionActions<CR>", { desc = "[C]ompanion [C]ompanion Actions" })
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
