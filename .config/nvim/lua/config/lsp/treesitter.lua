e1ad17cc0381b7e783f27d859141846d7a927b95
Ethan Walker <ethan.walker830@gmail.com>
2026-04-29 20:25:03 -0700

    fix argument swapping

--- .config/nvim/lua/config/lsp/treesitter.lua
+++ .config/nvim/lua/config/lsp/treesitter.lua
@@ -42,7 +42,7 @@ vim.keymap.set("n", "<leader>a", function()
   require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
 end)
 vim.keymap.set("n", "<leader>A", function()
-  require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
+  require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
 end)
 
 -- stylua: ignore start
